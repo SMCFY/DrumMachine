3f5bdd74e2f4a49c69950aac96cbdbf42e22a986
gericsapo <gericsapo@gmail.com>
2017-05-14 19:18:45 +0200

    i think this is working :)

--- Analyses/residual_extraction.m
+++ Analyses/residual_extraction.m
@@ -1,5 +1,5 @@
 % residual extraction by inverse filtering, based on mode analyses
-excLength = 2048; % excitation length
+excLength = 2^10; % excitation length
 
 [tom_x fs] = audioread('/Users/geri/Documents/Uni/SMC8/P8/DrumMachine/RecordingSess/Cropped_Samples/Tom_big/tom_big_pos1.aif');
 tom_res = tom_x(1:excLength);
@@ -14,15 +14,14 @@ for i=1:length(tom_big)
     %snare(i,1) = 2*pi*snare(i,1)/fs; % mode frequencies in radians/sample
 end
 
-
-R = 0.98; % pole radius
-
+R= 0.98;
 subplot(2,1,1);
 for i=1:length(tom_big)
         
+    %R = exp(-pi*tom_big(i,2)/fs);% pole radius
     b = [1 -2*cos(tom_big(i,1)) 1];        
-    a = [1 -2*R*cos(tom_big(i,1)) R^2];    
-        git
+    a = [1 -2*R*cos(tom_big(i,1)) R*R];    
+        
     tom_res = filter(b,a,tom_res); % apply inverse filter to get residual
     freqz(b,a); hold on;
 end
@@ -34,3 +33,5 @@ w = [0:excLength-1].*fs/excLength; %frequency in Hertz
 subplot(2,1,2);
 plot(w(1:length(w)/2), 20*log10(resMag(1:length(resMag)/2)));
 title('residual spectrum');
+
+sound(tom_res, fs)
