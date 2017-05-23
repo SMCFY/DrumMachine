0ee03662f51c4366f03c6124426de913ac2a1774
gericsapo <gericsapo@gmail.com>
2017-05-23 22:00:07 +0200

    i've created a monster(work in progress)

--- impactSynth.m
+++ impactSynth.m
@@ -18,16 +18,17 @@ classdef impactSynth < audioPlugin
     properties (Access = private)
         % vst parameters
         fs; % sampling rate
+        maxFrameSize; % maximum frame size in samples
         buff; % buffer for generated waveform
         t = 0.5; % buffer length in seconds
         N; % buffer in samples
         buffSum; % sum of all buffers (output frame)
-        readIndex = [1, 1, 1, 1]; % reading position in buffers
-        noteState = 'noteOff'; % note state
-        soundOut = [0, 0, 0, 0]; % state variable decides whether to output the signal from the buffer or not
+        readIndex = [1; 1; 1; 1]; % reading position in buffers
+        noteState = 'noteOff'; % trig
+        soundOut = [0; 0; 0; 0]; % state variable decides whether to output the signal from the buffer or not
 
         %=================================== synth parameters
-        modes = [];
+        modes = [180, 400, 600, 1250];
         %===================================
     end
     
@@ -44,52 +45,34 @@ classdef impactSynth < audioPlugin
 %--------------------------------------------------------------------------
     methods
         function obj = impactSynth() % constructor
-            obj.fs = (getSampleRate(obj));
+            obj.fs = getSampleRate(obj);
+            obj.maxFrameSize = 2048;
             obj.N = (0:1/obj.fs:obj.t); % number of samples length in seconds
             obj.buff = [zeros(1,length(obj.N));
                         zeros(1,length(obj.N));
                         zeros(1,length(obj.N));
                         zeros(1,length(obj.N))];
-            obj.bufSum = zeros(1,length(obj.N));
+            obj.buffSum = zeros(1,obj.maxFrameSize);
         end                                   
         
         function reset(obj)
             obj.fs = (getSampleRate(obj));
-            obj.readIndex = [1, 1, 1, 1];
-            obj.soundOut = [0, 0, 0, 0];
+            obj.maxFrameSize = 2048;
+            obj.readIndex = [1; 1; 1; 1];
+            obj.soundOut = [0; 0; 0; 0];
         end 
         
         function set.trig(obj, val)
             if val == 'noteOn_'
-                obj.readIndex(1,obj.instID) = 1; % init readIndex of respective buffer
-                obj.soundOut(1,obj.instID) = 1; % trigger sound according to instrument ID
+                obj.readIndex(obj.instID) = 1; % init readIndex of respective buffer
+                obj.soundOut(obj.instID) = 1; % trigger sound according to instrument ID
 
-                if obj.instID == 1
-                    fprintf('tom')
-                    %=================================== sound synthesis
-                    obj.buff(1,:) = bands(220, obj.N) + mesh(330, obj.N); % synthesis
-                    %===================================
-                elseif obj.instID == 2
-                    fprintf('snare')
-                    %=================================== sound synthesis
-                    obj.buff(1,:) = bands(440, obj.N) + mesh(442, obj.N)'; % synthesis
-                    %===================================
-                elseif obj.instID == 3
-                    fprintf('cymbal')
-                    %=================================== sound synthesis
-                    obj.buff(1,:) = bands(350, obj.N) + mesh(700, obj.N)'; % synthesis
-                    %=================================== 
-                else
-                    fprintf('kick')
-                    %=================================== sound synthesis
-                    obj.buff(1,:) = bands(120, obj.N) + mesh(180, obj.N)'; % synthesis
-                    %===================================
-                end
+                %=================================== sound synthesis
+                obj.buff(obj.instID,:) = bands(obj.modes(obj.instID), obj.N)'; % synthesis
+                %===================================
+
             end
-            buffSum = obj.buff(1,:) * obj.soundOut(1,1)...
-                    + obj.buff(2,:) * obj.soundOut(1,2)...
-                    + obj.buff(3,:) * obj.soundOut(1,2)...
-                    + obj.buff(4,:); % sum of all buffers
+
             obj.noteState = val;
         end 
         
@@ -98,25 +81,24 @@ classdef impactSynth < audioPlugin
         end 
         
         function out = process(obj, in) 
+            %if length(in) ~= obj.frameSize
+            %    obj.frameSize = length(in);
+            %end
 
-            %if obj.soundOut == 'true_' 
             for i=1:length(obj.soundOut)    
-                if obj.readIndex(1,i) < length(obj.buff(i,:)) - length(in) % buffer length not exceeded - output sound
-                    out = obj.buff(i,obj.readIndex(1,i):length(in)+obj.readIndex(1,i)-1)... % read from buffer
-                    * obj.soundOut(1,i); % applying state variable
-                    obj.readIndex(1,i) = obj.readIndex(1,i) + length(in); % increment readIndex
+                if obj.readIndex(i) < length(obj.buff(i,:)) - length(in) % buffer length not exceeded - output sound
+                    obj.buffSum(1:length(in)) = obj.buffSum(1:length(in)) + obj.buff(i,obj.readIndex(i):length(in)+obj.readIndex(i)-1)... % read from buffer
+                    * obj.soundOut(i); % applying state variable
+                    obj.readIndex(i) = obj.readIndex(i) + length(in); % increment readIndex
                 else % buffer length exceeded - output zeros
-                    obj.readIndex(1,i) = 1; % init readIndex
-                    obj.soundOut(1,i) = 0;
-                    out = zeros(length(in),1);
+                    obj.readIndex(i) = 1; % init readIndex
+                    obj.soundOut(i) = 0;
+                    obj.buffSum(1:length(in)) = obj.buffSum(1:length(in)) + zeros(1,length(in));
                 end
             end    
-            %else
 
-                %out = zeros(length(in),1);
-                
-            %end
-            
+                out = obj.buffSum(1:length(in))'; %output the sum of all instrument buffers
+
         end  
     end
 %--------------------------------------------------------------------------
